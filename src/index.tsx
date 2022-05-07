58d85993884a46304189fd5a41de8bbf04046385
Alexey Egorov <krosy1337@gmail.com>
2022-05-07 11:41:55 +0300

    Added local storage for app mode

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from "react-redux"
-import {store} from "./store/store"
+import {persistor, store} from "./store/store"
+import {PersistGate} from "redux-persist/integration/react"
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -12,7 +13,9 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
-                <App/>
+                <PersistGate persistor={persistor}>
+                    <App/>
+                </PersistGate>
             </Provider>
         </BrowserRouter>
     </React.StrictMode>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import ticTacToeReducer from "./slices/ticTacToeSlice"
 import appReducer from "./slices/appSlice"
+import {
+    persistStore, persistReducer, FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+    ticTacToe: ticTacToeReducer,
+    app: appReducer,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['app'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        ticTacToe: ticTacToeReducer,
-        app: appReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
